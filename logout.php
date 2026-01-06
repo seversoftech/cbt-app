79ff4bc13db9da3ce90e94fb5c187638162b3c44
seversoftech <developersever@gmail.com>
2026-01-06 04:32:57 +0100

    ..

--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@ session_start();
 session_unset();          
 session_destroy();        
 
-// Optional
+// Optional.
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
